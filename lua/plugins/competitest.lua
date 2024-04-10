a8e5bce5c5bcc5aaba485835bf01b381971f8a25
meng <1792766874@qq.com>
2024-04-10 18:54:09 +0800

    add plugins competitest

---
+++ lua/plugins/competitest.lua
@@ -0,0 +1,11 @@
+local M = {
+  {
+    "xeluxee/competitest.nvim",
+    dependencies = "MunifTanjim/nui.nvim",
+    config = function()
+      require("competitest").setup()
+    end,
+  },
+}
+
+return M
