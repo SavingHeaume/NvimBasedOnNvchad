cb3ffcfdfc9e2e453f12eed71733dfa5c23811f6
meng <1792766874@qq.com>
2024-04-20 21:39:20 +0800

    添加diffview插件

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,20 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		config = function()
-			require("configs.conform")
-		end,
-	},
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require "configs.conform"
+    end,
+  },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		opts = {
-			git = { enable = true },
-		},
-	},
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      git = { enable = true },
+    },
+  },
+
+  {
+    "sindrets/diffview.nvim",
+    event = "User FilePost",
+  },
 }
--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -4,38 +4,38 @@ local home = ""
 local cache = ""
 
 if is_windows then
-	home = "D:\\Project\\LeetCode"
-	cache = "D:\\Project\\LeetCode"
+  home = "D:\\Project\\LeetCode"
+  cache = "D:\\Project\\LeetCode"
 else
-	home = "/mnt/d/Project/LeetCode"
-	cache = "/mnt/d/Project/LeetCode"
+  home = "/mnt/d/Project/LeetCode"
+  cache = "/mnt/d/Project/LeetCode"
 end
 
 local leetcode = {
-	{
-		"kawre/leetcode.nvim",
-		build = ":TSUpdate html",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-			"nvim-lua/plenary.nvim", -- required by telescope
-			"MunifTanjim/nui.nvim",
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
 
-			-- optional
-			"nvim-treesitter/nvim-treesitter",
-			"rcarriga/nvim-notify",
-			"nvim-tree/nvim-web-devicons",
-		},
-		lazy = leet_arg ~= vim.fn.argv()[1],
-		opts = {
-			storage = {
-				home = home,
-				cache = cache,
-			},
-			cn = {
-				enabled = true,
-			},
-		},
-	},
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = leet_arg ~= vim.fn.argv()[1],
+    opts = {
+      storage = {
+        home = home,
+        cache = cache,
+      },
+      cn = {
+        enabled = true,
+      },
+    },
+  },
 }
 
 return leetcode
