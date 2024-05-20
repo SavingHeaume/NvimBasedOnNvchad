ec358527d2621002de666c8c5fedcfd1b8c434c8
meng <1792766874@qq.com>
2024-05-20 21:43:34 +0800

    修改主题

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,10 +1,26 @@
 local M = {}
 
 M.ui = {
-	theme = "wombat",
-	nvdash = {
-		load_on_startup = true,
-	},
+  theme = "wombat",
+
+  cmp = {
+    -- default/flat_light/flat_dark/atom/atom_colored
+    style = "atom",
+  },
+
+  nvdash = {
+    load_on_startup = true,
+  },
+
+  telescope = {
+    -- borderless / bordered
+    style = "borderless",
+  },
+
+  statusline = {
+    -- default/vscode/vscode_colored/minimal
+    theme = "minimal",
+  },
 }
 
 return M
