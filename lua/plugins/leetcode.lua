5f17893466cc1bd0ebc69086c15d63da0125cfd0
meng <1792766874@qq.com>
2024-08-02 17:11:59 +0800

    feat: leetcode.nvim

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,41 @@
+local leet_arg = "leetcode.nvim"
+local is_windows = package.config:sub(1, 1) == "\\"
+local home = ""
+local cache = ""
+
+if is_windows then
+  home = "D:\\Projects\\LeetCode"
+  cache = "D:\\Projecs\\LeetCode"
+else
+  home = "~/Projects/LeetCode"
+  cache = "~/Projects/LeetCode"
+end
+
+local leetcode = {
+  {
+    "SavingHeaume/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = leet_arg ~= vim.fn.argv()[1],
+    opts = {
+      storage = {
+        home = home,
+        cache = cache,
+      },
+      cn = {
+        enabled = true,
+      },
+    },
+  },
+}
+
+return leetcode
