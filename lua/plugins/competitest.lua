975bdd2acca194c4929966f7cd133c3c605bb40d
meng <1792766874@qq.com>
2024-08-02 10:47:07 +0800

    refactor

--- lua/plugins/competitest.lua
+++ lua/plugins/competitest.lua
@@ -1,5 +1,3 @@
-local map = vim.keymap.set
-
 local M = {
   {
     "xeluxee/competitest.nvim",
--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -1,43 +1,43 @@
 local plugins = {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				-- defaults
-				"vim",
-				"lua",
-				"vimdoc",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "vim",
+        "lua",
+        "vimdoc",
 
-				-- low level
-				"cpp",
-				"c",
-				"python",
-				"java",
+        -- low level
+        "cpp",
+        "c",
+        "python",
+        "java",
         "markdown",
         "rust",
-			},
-		},
-	},
+      },
+    },
+  },
 
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"lua-language-server",
-				"stylua",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
 
-				"clangd",
-				"clang-format",
-				"cmake-language-server",
-				"codelldb",
+        "clangd",
+        "clang-format",
+        "cmake-language-server",
+        "codelldb",
 
-				"pyright",
+        "pyright",
 
-				"jdtls",
+        "jdtls",
         "rust-analyzer",
-			},
-		},
-	},
+      },
+    },
+  },
 }
 
 return plugins
