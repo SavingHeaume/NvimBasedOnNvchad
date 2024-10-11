9e3a76492ff341da011b7a798e303c1ea12d1308
meng <1792766874@qq.com>
2024-10-11 14:23:51 +0800

    feat: 添加menu快捷键

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -43,3 +43,15 @@ end, { desc = "打开 REPL" })
 map("n", "<Leader>dl", function()
   require("dap").run_last()
 end, { desc = "运行最后一次调试配置" })
+
+-- menu
+vim.keymap.set("n", "<C-t>", function()
+  require("menu").open "default"
+end, {})
+
+vim.keymap.set("n", "<RightMouse>", function()
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
+  require("menu").open(options, { mouse = true })
+end, {})
